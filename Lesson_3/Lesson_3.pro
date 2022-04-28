bc92e1f7c37b5206a64dce5b801d33c5a14db109
Alexander Yaroslavtcev <YaroslavtcevAV@gmail.com>
2022-04-28 08:45:55 +0300

    first task of third lesson

---
+++ Lesson_3/Lesson_3.pro
@@ -0,0 +1,33 @@
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+TARGET = NoteBook
+TEMPLATE = app
+CONFIG += c++17
+
+# You can make your code fail to compile if it uses deprecated APIs.
+# In order to do so, uncomment the following line.
+#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+SOURCES += \
+    help_form.cpp \
+    main.cpp \
+    mainwindow.cpp \
+    notebook.cpp
+
+HEADERS += \
+    help_form.h \
+    mainwindow.h \
+    notebook.h
+
+FORMS += \
+    help_form.ui \
+    mainwindow.ui
+
+# Default rules for deployment.
+qnx: target.path = /tmp/$${TARGET}/bin
+else: unix:!android: target.path = /opt/$${TARGET}/bin
+!isEmpty(target.path): INSTALLS += target
+
+RESOURCES += \
+    resources.qrc
---
+++ Lesson_3/mainwindow.cpp
@@ -0,0 +1,59 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QFile>
+#include <QTextStream>
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+
+
+//actions
+void MainWindow::on_action_exit_triggered()
+{
+    this->close();
+}
+
+void MainWindow::on_action_help_triggered()
+{
+    help = new HelpForm(this);
+    help->show();
+
+}
+
+void MainWindow::on_action_open_triggered()
+{
+    ui->textEdit->setText(note.openFile(ui->textEdit->toPlainText()));
+}
+
+//buttons
+void MainWindow::on_btn_help_clicked()
+{
+    ui->textEdit->setText(note.setUnicodeSymbols(ui->textEdit->toPlainText()));
+}
+
+void MainWindow::on_btn_open_clicked()
+{
+    this->on_action_open_triggered();
+}
+
+
+void MainWindow::on_btn_save_clicked()
+{
+    note.saveFile(ui->textEdit->toPlainText());
+}
+
+
