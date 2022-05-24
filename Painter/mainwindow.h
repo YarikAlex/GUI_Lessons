bbd385d08cc72ea576a620db2333d3614e90a7f3
Alexander Yaroslavtcev <YaroslavtcevAV@gmail.com>
2022-05-24 12:24:30 +0300

    Exercise 2.

---
+++ Painter/mainwindow.h
@@ -0,0 +1,40 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QPainter>
+#include <QPen>
+#include <QBrush>
+#include <QMouseEvent>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    ushort _count = 0;
+    QPainter _painter;
+    QPen _pen;
+    QBrush _brush;
+    const QRectF _rectangle{50, 60, 200, 150};
+    const QPointF _star[5] = {QPointF(150, 10),
+                             QPointF(67, 253),
+                             QPointF(283, 106),
+                             QPointF(17, 106),
+                             QPointF(232, 253)};
+
+protected:
+    void paintEvent(QPaintEvent*) override;
+    void mousePressEvent(QMouseEvent* event) override;
+
+};
+#endif // MAINWINDOW_H
